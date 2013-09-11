9e34d30637347709fedc0e679b66f2cf03a71ece
akabeko <akabekobeko@gmail.com>
2013-09-12 00:31:31 +0900

    #141 リリース用イメージ作成スクリプトを修正

--- release.sh
+++ release.sh
@@ -9,7 +9,11 @@ cp style.css        ${TARGETDIR}
 cp wp-imageomap.js  ${TARGETDIR}
 cp wp-imageomap.php ${TARGETDIR}
 cp exif-reader.php  ${TARGETDIR}
-cp README.txt       ${TARGETDIR}
+cp readme.txt       ${TARGETDIR}
 cp -r pel           ${TARGETDIR}
 cp -r languages     ${TARGETDIR}
 cp images/*.png     ${TARGETDIR}/images
+
+find ${TARGETDIR} -name ".DS_Store" -print -exec rm {} ";"
+zip -r ${TARGETDIR}.zip ${TARGETDIR}
+rm -rf ${TARGETDIR}
